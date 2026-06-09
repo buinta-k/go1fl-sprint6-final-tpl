6a0d44079dafa664b9ed84f0db311d0de92420c8
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-06-09 15:36:46 +0300

    Update handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -16,44 +16,63 @@ func HandleOne(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Файл не найден", http.StatusNotFound)
 		return
 	}
-	w.Header().Set("Content-Type", "text/html; charset-utf-8")
+	
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	w.Write(data)
 }
 
 func HandleTwo(w http.ResponseWriter, req *http.Request) {
+	var inputString string
+
 	file, header, err := req.FormFile("file")
-	if err != nil {
-		http.Error(w, "", http.StatusBadRequest)
-		return
+	
+	if err == nil {
+		defer file.Close() 
+		fileBytes, readErr := io.ReadAll(file)
+		if readErr == nil {
+			inputString = string(fileBytes)
+		}
 	}
-	defer file.Close()
 
-	fileBytes, err := io.ReadAll(file)
-	if err != nil {
-		http.Error(w, "Ошибка чтения файла", http.StatusInternalServerError)
-		return
+	if inputString == "" {
+		bodyBytes, err := io.ReadAll(req.Body)
+		
+		if err != nil || len(bodyBytes) == 0 {
+			http.Error(w, "Данные запроса пусты", http.StatusBadRequest)
+			return
+		}
+		inputString = string(bodyBytes)
 	}
-	inputString := string(fileBytes)
 
+	
 	resultString, err := service.Converter(inputString)
 	if err != nil {
 		http.Error(w, "Ошибка конвертации: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	ext := filepath.Ext(header.Filename)
-	currentTime := time.Now().UTC().String()
+	var ext string
+	if header != nil {
+		ext = filepath.Ext(header.Filename)
+	}
 
+	if ext == "" {
+		ext = ".txt"
+	}
+
+
+	currentTime := time.Now().UTC().Format("20060102150405")
 	newFileName := currentTime + ext
 
 	err = os.WriteFile(newFileName, []byte(resultString), 0644)
 	if err != nil {
 		http.Error(w, "Не удалось сохранить файл на сервер", http.StatusInternalServerError)
+		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain; charset-utf-8")
-
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(resultString))
 }
 
