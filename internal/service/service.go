69f8c7f9f9261148ac83da0ffc86cf7fa165f3bf
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-06-09 13:33:19 +0300

    Update service.go

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,14 +2,20 @@ package service
 
 import (
 	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
 )
 
-func main() {
+func Converter(s string) (string, error) {
 	Text := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+	var res string
+
 	if strings.ContainsAny(s, Text) {
-		res := ToText(s)
+		res = morse.ToText(s)
 	} else {
-		res := ToMorse(s)
+		res = morse.ToMorse(s)
 	}
+
+	return res, nil
 }
