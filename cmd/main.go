69055399c45a84440f0ed3e39ac1161e32cffee8
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-06-10 16:28:50 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
+
+	logger.Println("Инициализация настроек сервера...")
+
+	srv := server.New(logger)
+
+	err := srv.Start()
+
+	if err != nil {
+		logger.Fatalf("Ошибка %v", err)
+	}
+	
 }
