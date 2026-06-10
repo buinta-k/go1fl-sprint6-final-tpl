9f69639909685cf95e95b1ff80c1706f12786f43
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-06-10 16:31:35 +0300

    Update handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -51,4 +51,5 @@ func HandleTwo(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(resultString))
+	
 }
