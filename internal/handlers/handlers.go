1f556fd5b58b34190f6f171273b2c288bbb9e41d
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-06-10 16:29:15 +0300

    Update handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,54 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func HandleOne(w http.ResponseWriter, req *http.Request) {
+	data, err := os.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write(data)
+}
+
+func HandleTwo(w http.ResponseWriter, req *http.Request) {
+	file, header, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	resultString, err := service.Converter(string(fileBytes))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	newFileName := time.Now().UTC().String() + filepath.Ext(header.Filename)
+
+	err = os.WriteFile(newFileName, []byte(resultString), 0644)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(resultString))
+}
